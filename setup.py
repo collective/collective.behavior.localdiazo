ad1147c088af148635e98ea6b1acbe559c03613d
hvelarde <hector.velarde@gmail.com>
2013-08-15 11:09:52 -0300

    Back to development: 1.0b3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 import os
 
-version = '1.0b2'
+version = '1.0b3.dev0'
 long_description = (
     open("README.rst").read() + "\n" +
     open(os.path.join("docs", "CREDITS.rst")).read() + "\n" +
